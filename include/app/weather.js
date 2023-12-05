d4ccde370bc3fc414341f1514368b596537c367a
Adam Karski <zbigniew.karski@gmail.com>
2023-12-05 16:09:29 +0100

    latest

--- include/app/weather.js
+++ include/app/weather.js
@@ -1,16 +1,15 @@
-
 let owapikey = '5d652f0bff80d46c6a9ce422c48e4296';
 let lat = '49.88087';
 let lon = '19.56303';
-
 let data = {};
+let srcOfImage = "";
+
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let convertTimestamp = (timestamp, type) => {
-
+function convertTimestamp(timestamp, type) {
   let date = new Date(timestamp * 1000);
   let hours = date.getHours();
   let minutes = date.getMinutes();
@@ -43,33 +42,25 @@ let convertTimestamp = (timestamp, type) => {
   else if (type == "seconds") {
     return seconds;
   } else return false;
-};
-
-let srcOfImage = "";
-
+}
 
 // get forecast
 function getForecast() {
   fetch(
     // new API 
     "//api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + owapikey + "&units=metric"
-
   )
     .then((response) => response.json())
     .then((data_) => {
 
-   
-     console.log(data_.current.temp);
+      // console.log(data_.current.temp);
       if (data_.current.temp == '-0') { data_.current.temp = 0 }
       //data = data_;
       srcOfImage = "//openweathermap.org/img/wn/" + data_.current.weather[0].icon + "@2x.png";
-
       document.querySelector('.ifdata').classList.toggle('active');
       $("footer .a_weather").append(`<img  src="//${srcOfImage}" alt=""/>`)
       $("footer .a_weather").show();
       $('.container-modal').hide();
-    
-
       $('.weather_widget .close, footer .a_weather').bind('click', function () {
         $('.container-modal').toggle(300);
       });
@@ -78,30 +69,19 @@ function getForecast() {
       nextHours(data_['hourly']).then(() => {
 
         const swiper = new Swiper('.swiper', {
- 
-grabCursor: true,
-//centeredSlides: true,
-//slidesPerView: 1,
-
-watchSlidesProgress: true,
-slidesPerView: 3,
-
-autoplay: {
-  delay: 4500,
-  disableOnInteraction: true
-}
-
+          grabCursor: true,
+          watchSlidesProgress: true,
+          slidesPerView: 3,
+          autoplay: {
+            delay: 4500,
+            disableOnInteraction: true
+          }
 
         });
 
       }).then(
-
         nextDays(data_['daily'])
-
       );
-
-
-
     });
 }
 
@@ -117,10 +97,10 @@ let nextHours = async (data) => {
     data.forEach(nexthour => {
 
       let el = `<div class="swiper-slide flex time_items flex-col items-center noSelect" style="margin-right:30px; padding-bottom: 20px;">
-              <span class="font-semibold mt-1 text-sm lcd noSelect time_dis">${convertTimestamp(nexthour.dt, "hours")}:${convertTimestamp(nexthour.dt, "minutes")} </span>
-             <span class="text-xs font-semibold text-gray-400 lcd noSelect">PM</span>       
+                <span class="font-semibold mt-1 text-sm lcd noSelect time_dis">${convertTimestamp(nexthour.dt, "hours")}:${convertTimestamp(nexthour.dt, "minutes")} </span>
+                <span class="text-xs font-semibold text-gray-400 lcd noSelect AMPM">PM</span>       
                 <img alt="" src="////openweathermap.org/img/wn/${nexthour.weather[0].icon}@2x.png" width="120px" class="noSelect" style="pointer-events: none;"/>	 
-              <span class="font-semibold text-lg lcd noSelect">${nexthour.temp.toFixed(1)}°C</span>      <span class="text-sm lcd noSelect">${nexthour.feels_like}</span></div>`;
+                <span class="font-semibold text-lg lcd noSelect">${nexthour.temp.toFixed(1)}°C</span>      <span class="text-sm lcd noSelect">${nexthour.feels_like}</span></div>`;
       html = html + el;
     });
     $('.nextHours').html("").append(html + `</div>`);
@@ -135,24 +115,19 @@ let currentHour = async (data) => {
     console.log(data);
 
     html = `<div class="flex flex-col">
-        <span class="text-3xl font-bold lcd">${data.current.temp}<span class="text-3xl">°C</span></span>
-        <span class="font-light mt-1 text-gray-500">Odczuwalna : ${data.current.feels_like.toFixed(1)}</span>
-        <!-- <span class="font-semibold mt-1 text-gray-500">${data.name}</span> -->
-      </div>
-      <div class="flex flex-col">
-        <span class="text-3xl font-bold lcd">${data.current.humidity.toFixed(0)}<span class="text-3xl"><span
-              class="upperTop">o</span> /o</span></span>
+            <span class="text-3xl font-bold lcd">${data.current.temp}<span class="text-3xl oc">°C</span></span>
+            <span class="font-light mt-1 text-gray-500 odczuwalna">Odczuwalna : ${data.current.feels_like.toFixed(1)}</span>
+            <!-- <span class="font-semibold mt-1 text-gray-500">${data.name}</span> -->
+            </div>
+            <div class="flex flex-col">
+            <span class="text-3xl font-bold lcd">&nbsp${data.current.humidity.toFixed(0)}<span class="text-3xl"><span
+              class="upperTop">o</span>/o</span></span>
 
-        <!-- <span class="font-semibold mt-1 text-gray-500">${data.name}</span> -->
-      </div>
-      <div class="h-24 w-24 ">
-       
-
-        <img  src="//${srcOfImage}" alt="" class="currentWeatherIcon"/>
-</div>`;
-
-
-
+            <!-- <span class="font-semibold mt-1 text-gray-500">${data.name}</span> -->
+            </div>
+            <div class="h-24 w-24 ">
+            <img  src="//${srcOfImage}" alt="" class="currentWeatherIcon"/>
+            </div>`;
     $('.currentHour').html("").append(html);
     return true;
   }
@@ -162,55 +137,35 @@ let currentHour = async (data) => {
 let nextDays = async (data) => {
 
   if (data) {
-
     let html;
 
     data.forEach(day => {
-
       el = `<div class="flex justify-between items-center">
-        <span class="font-semibold text-sm w-1/4 firstLetterUp">
-          ${dayjs.unix(day.dt).format('dddd')}<br>
-          ${dayjs.unix(day.dt).locale('pl').format('D')} ${dayjs.unix(day.dt).format('MMM')}
-        </span>
-        <div class="flex items-center justify-end w-1/4 pr-10">
-         <span class="font-semibold humidityForecast">
-           ${day.humidity}%
-        </span>
-
-          <IconHumidity></IconHumidity>
-
-        </div>
-        <img alt="" src="////openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" width="" />	
-
-        <span class="font-semibold text-lg w-1/4 text-right temp_primary">${day.temp.day}°</span>
-        <span class="under">
-
-          <span class="fontTemp w-1/2 text-right temp_secondary"><span class="labelTemp">min
-            </span>${day.temp.min}°</span>
-          <span class="fontTemp w-1/2 text-right temp_secondary"><span class="labelTemp">max
-            </span>${day.temp.max}°</span>
-        </span>
-      </div>`
-
+              <span class="font-semibold text-sm w-1/4 firstLetterUp">
+              ${dayjs.unix(day.dt).format('dddd')}<br>
+              ${dayjs.unix(day.dt).locale('pl').format('D')} ${dayjs.unix(day.dt).format('MMM')}
+              </span>
+            <div class="flex items-center justify-end w-1/4 pr-10">
+                <span class="font-semibold humidityForecast">
+                ${day.humidity}%
+                </span>
+            <IconHumidity></IconHumidity>
+            </div>
+              <img alt="" src="////openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" width="" />	
+              <span class="font-semibold text-lg w-1/4 text-right temp_primary">${day.temp.day}°</span>
+              <span class="under">
+              <span class="fontTemp w-1/2 text-right temp_secondary"><span class="labelTemp">min
+              </span>${day.temp.min}°</span>
+              <span class="fontTemp w-1/2 text-right temp_secondary"><span class="labelTemp">max
+              </span>${day.temp.max}°</span>
+              </span>
+            </div>`
       html = html + el;
     })
-
-
-
-
     $('.nextDays').html("").append(html);
     return true;
-
   } else { return false }
-
-
-
-
 }
-
-
-
-
 // setInterval(getForecast, 20000) // 33,33 minuty 
 
 
